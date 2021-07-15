cedf1affcc0608a9f44ff29d60ec06ba85208cc5
Jon Andoni Galdos <jonandonigaldos1996@gmail.com>
2021-07-15 14:53:57 +0200

    load data

--- sentiment-analysis-project.py
+++ sentiment-analysis-project.py
@@ -1,6 +1,31 @@
+import os
+import random
+
 def load_training_data(
-    data_directory: str = "aclImdb_v1/aclImdb/train", 
+    data_directory: str = "data/aclImdb_v1/aclImdb/train", 
     split: float = 0.8, 
     limit: int = 0
 ) -> tuple:
-    pass
+    # Load from files
+    reviews = []
+    for label in ["pos", "neg"]:
+        labeled_directory = f"{data_directory}/{label}"
+        for review in os.listdir(".txt"):
+            if review.endswith(".txt"):
+                with open(f"{labeled_directory}/{review}") as f:
+                    text = f.read()
+                    text = text.replace("<br />", "\n\n")
+                    if text.strip():
+                        spacy_label = {
+                            "cats": {
+                                "pos": "pos" == label,
+                                "neg": "neg" == label
+                            }
+                        }
+                        reviews.append((text, spacy_label))
+    random.shuffle(reviews)
+
+    if limit:
+        reviews = reviews[:limit]
+    split = int(len(reviews) * split)
+    return reviews[:split], reviews[split:]
